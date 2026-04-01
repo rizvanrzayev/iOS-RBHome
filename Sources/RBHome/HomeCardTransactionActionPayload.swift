58b231f12bf3513d47d34e18db317063052d9d3f
Rizvan Rzayev <r.i.rzayev@rabita.com>
2026-04-01 15:37:31 +0400

    chore: re-export home statement payload

---
+++ Sources/RBHome/HomeCardTransactionActionPayload.swift
@@ -0,0 +1,10 @@
+//
+//  HomeCardTransactionActionPayload.swift
+//  RBHome
+//
+//  Created by Rizvan Rzayev on 01.04.2026.
+//
+
+import RBHomeDomain
+
+public typealias HomeCardTransactionActionPayload = RBHomeDomain.HomeCardTransactionActionPayload
